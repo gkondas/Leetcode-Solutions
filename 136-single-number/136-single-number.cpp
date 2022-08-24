68d5a7a03af79677ec3ecec9100272e28e5300c1
Gregory Kondas <59902952+gkondas@users.noreply.github.com>
2022-08-24 17:50:31 -0400

    Time: 27 ms (69.95%), Space: 17 MB (52.94%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int out =0;
+        for(auto num:nums){
+            out ^= num;
+        }
+        
+        return out;
+    }
+};
